81e4aac45a08734b0667c0a918eec09ca299a36a
reginmund9 <var23004@byui.edu>
2024-01-23 20:37:58 -0600

    long

---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+const outputDate = document.querySelector("#current-year");
+const outputModified = document.querySelector("#last-modified");
+  
+const date = new Date().getFullYear();
+let lastModified = document.lastModified;
+
+outputDate.textContent = date;
+outputModified.textContent = lastModified;
