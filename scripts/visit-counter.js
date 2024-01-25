74b1addf20d5196d48d0bedfbca2c9601beca9f2
reginmund9 <var23004@byui.edu>
2024-01-25 02:46:54 -0600

    rather

---
+++ scripts/visit-counter.js
@@ -0,0 +1,9 @@
+
+const visitCount = document.querySelector('#visit-count');
+
+let totalVisits = Number(window.localStorage.getItem('visits-total'));
+totalVisits++;
+localStorage.setItem('visits-total', totalVisits);
+  
+visitCount.textContent = totalVisits;
+
